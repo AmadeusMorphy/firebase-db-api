59aab2766a1ec89be6c37d25ae7fbf90e909b35d
NizarStuff <nizarmasadeh2001@gmail.com>
2024-10-14 00:28:14 +0300

    noshit

--- index.js
+++ index.js
@@ -1,23 +1,29 @@
-const express = require('express');
 const admin = require('firebase-admin');
+
+admin.initializeApp({
+    credential: admin.credential.cert({
+        type: process.env.FIREBASE_TYPE,
+        project_id: process.env.FIREBASE_PROJECT_ID,
+        private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+        private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '8bfa23cd8cf6f89c103158816e7ed8be99191d65'),
+        client_email: process.env.FIREBASE_CLIENT_EMAIL,
+        client_id: process.env.FIREBASE_CLIENT_ID,
+        auth_uri: process.env.FIREBASE_AUTH_URI,
+        token_uri: process.env.FIREBASE_TOKEN_URI,
+        auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+        client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL
+    })
+});
+
+const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Initialize Firebase
-const serviceAccount = require('./mrnzd-d0f4d-firebase-adminsdk-pebej-8bfa23cd8c.json');
-
-admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-});
-
-const db = admin.firestore();
-
-// Set up a basic route
 app.get('/data', async (req, res) => {
     try {
-        const snapshot = await db.collection('users').get();
+        const snapshot = await admin.firestore().collection('users').get();
         const data = snapshot.docs.map(doc => doc.data());
-        res.status(200).send(data);
+        res.status(200).json(data);
     } catch (error) {
         res.status(500).send('Error retrieving data: ' + error.message);
     }
